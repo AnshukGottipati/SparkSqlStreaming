489b0f264a22964bd805a01300245e2641c8a611
AnshukGottipati <92184851+AnshukGottipati@users.noreply.github.com>
2025-10-31 17:09:58 +0000

    task1 Push

--- task1.py
+++ task1.py
@@ -1,5 +1,5 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col
+from pyspark.sql.functions import from_json, col, to_timestamp
 from pyspark.sql.types import StructType, StructField, StringType, DoubleType, TimestampType
 
 # Create a Spark session
@@ -15,9 +15,32 @@ schema = StructType([
 ])
 
 # Read streaming data from socket
-
+df = (
+    spark.readStream
+         .format("socket")
+         .option("host", "localhost")
+         .option("port", 9999)
+         .load()
+)
 # Parse JSON data into columns using the defined schema
-
+parse_df = (
+    df
+    .select(from_json(col("value"), schema).alias("json"))
+    .select(
+        col("json.trip_id"),
+        col("json.driver_id"),
+        col("json.distance_km"),
+        col("json.fare_amount"),
+        to_timestamp(col("json.timestamp")).alias("timestamp")
+    )
+)
 # Print parsed data to the CSV files
+query = (
+    parse_df.writeStream
+    .format("console")
+    .option("truncate", "false")
+    .outputMode("append")
+    .start()
+)
 
 query.awaitTermination()
